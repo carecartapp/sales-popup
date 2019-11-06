ed2c75e5434ae8c1c792a4a2cfd9e6ee946be520
CareCart Shopify APP <44872750+carecartapp@users.noreply.github.com>
2019-11-06 16:00:37 +0500

    Added legacy support

--- salesnotifier.js
+++ salesnotifier.js
@@ -1,4 +1,4 @@
-//CDN Version 1.0.12
+//CDN Version 1.0.13
 
 function scriptInjection(src, callback) {
 	var script = document.createElement('script');
@@ -650,8 +650,7 @@ scriptInjection("https://code.jquery.com/jquery-3.2.1.min.js", function () {
 		($jq321.browser = $jq321.browser || {}).mobile = /(android|bb\d+|meego).+mobile|avantgo|bada\/|blackberry|blazer|compal|elaine|fennec|hiptop|iemobile|ip(hone|od)|iris|kindle|lge |maemo|midp|mmp|mobile.+firefox|netfront|opera m(ob|in)i|palm( os)?|phone|p(ixi|re)\/|plucker|pocket|psp|series(4|6)0|symbian|treo|up\.(browser|link)|vodafone|wap|windows (ce|phone)|xda|xiino/i.test(a) || /1207|6310|6590|3gso|4thp|50[1-6]i|770s|802s|a wa|abac|ac(er|oo|s\-)|ai(ko|rn)|al(av|ca|co)|amoi|an(ex|ny|yw)|aptu|ar(ch|go)|as(te|us)|attw|au(di|\-m|r |s )|avan|be(ck|ll|nq)|bi(lb|rd)|bl(ac|az)|br(e|v)w|bumb|bw\-(n|u)|c55\/|capi|ccwa|cdm\-|cell|chtm|cldc|cmd\-|co(mp|nd)|craw|da(it|ll|ng)|dbte|dc\-s|devi|dica|dmob|do(c|p)o|ds(12|\-d)|el(49|ai)|em(l2|ul)|er(ic|k0)|esl8|ez([4-7]0|os|wa|ze)|fetc|fly(\-|_)|g1 u|g560|gene|gf\-5|g\-mo|go(\.w|od)|gr(ad|un)|haie|hcit|hd\-(m|p|t)|hei\-|hi(pt|ta)|hp( i|ip)|hs\-c|ht(c(\-| |_|a|g|p|s|t)|tp)|hu(aw|tc)|i\-(20|go|ma)|i230|iac( |\-|\/)|ibro|idea|ig01|ikom|im1k|inno|ipaq|iris|ja(t|v)a|jbro|jemu|jigs|kddi|keji|kgt( |\/)|klon|kpt |kwc\-|kyo(c|k)|le(no|xi)|lg( g|\/(k|l|u)|50|54|\-[a-w])|libw|lynx|m1\-w|m3ga|m50\/|ma(te|ui|xo)|mc(01|21|ca)|m\-cr|me(rc|ri)|mi(o8|oa|ts)|mmef|mo(01|02|bi|de|do|t(\-| |o|v)|zz)|mt(50|p1|v )|mwbp|mywa|n10[0-2]|n20[2-3]|n30(0|2)|n50(0|2|5)|n7(0(0|1)|10)|ne((c|m)\-|on|tf|wf|wg|wt)|nok(6|i)|nzph|o2im|op(ti|wv)|oran|owg1|p800|pan(a|d|t)|pdxg|pg(13|\-([1-8]|c))|phil|pire|pl(ay|uc)|pn\-2|po(ck|rt|se)|prox|psio|pt\-g|qa\-a|qc(07|12|21|32|60|\-[2-7]|i\-)|qtek|r380|r600|raks|rim9|ro(ve|zo)|s55\/|sa(ge|ma|mm|ms|ny|va)|sc(01|h\-|oo|p\-)|sdk\/|se(c(\-|0|1)|47|mc|nd|ri)|sgh\-|shar|sie(\-|m)|sk\-0|sl(45|id)|sm(al|ar|b3|it|t5)|so(ft|ny)|sp(01|h\-|v\-|v )|sy(01|mb)|t2(18|50)|t6(00|10|18)|ta(gt|lk)|tcl\-|tdg\-|tel(i|m)|tim\-|t\-mo|to(pl|sh)|ts(70|m\-|m3|m5)|tx\-9|up(\.b|g1|si)|utst|v400|v750|veri|vi(rg|te)|vk(40|5[0-3]|\-v)|vm40|voda|vulc|vx(52|53|60|61|70|80|81|83|85|98)|w3c(\-| )|webc|whit|wi(g |nc|nw)|wmlb|wonu|x700|yas\-|your|zeto|zte\-/i.test(a.substr(0, 4))
 	})(navigator.userAgent || navigator.vendor || window.opera);
 
-	// @todo below custom code block can be replaced with
-	// var salespoplib_active_url = location.hostname + location.pathname
+	// @todo below custom code block can be replaced with: var salespoplib_active_url = location.hostname + location.pathname
 	var salespoplib_active_url = String(location.href).replace('http://', '').replace('https://', '').replace('www.', '');
 	salespoplib_active_url = (salespoplib_active_url.substr(-1) == "/") ? salespoplib_active_url.substr(0, salespoplib_active_url.length - 1) : salespoplib_active_url;
 
@@ -661,17 +660,38 @@ scriptInjection("https://code.jquery.com/jquery-3.2.1.min.js", function () {
 	domain_grabber = (domain_grabber.substring(0, 4) == "www.") ? domain_grabber.substr(4) : domain_grabber;	// minus www. if exists
 	domain_grabber = (domain_grabber.substr(-1) == "/") ? domain_grabber.substr(0, domain_grabber.length - 1) : domain_grabber;
 
-	var allScripts = document.getElementsByTagName('script');
-	allScripts = Array.prototype.slice.call(allScripts);
-	var lib = "";
-	allScripts.forEach(function (element) {
-		if (element.src && element.src.indexOf('lib/salesnotifier') !== -1) {
-			lib = element.src;
-		}
-	});
+	function getServerUrls() {
+		// Finding the URL of this library among all the script tags
+		var allScripts = document.getElementsByTagName('script');
+		allScripts = Array.prototype.slice.call(allScripts);
 
-	var backendUri = "https://tracking-sales-pop.carecart.io/FrontController/";
-	var cssUri = "https://sales-pop.carecart.io/public/front_assets/new-ui/css/notif-box.css";
+		var thisLibUrl = "";
+		allScripts.forEach(function (script) {
+			if (script.src && script.src.indexOf('lib/salesnotifier') !== -1) {
+				thisLibUrl = script.src;
+			}
+		});
+
+		// return with production URLs
+		if( thisLibUrl === "" ) {
+			return {
+				"backend": "https://tracking-sales-pop.carecart.io/FrontController/",
+				"css": "https://sales-pop.carecart.io/public/front_assets/new-ui/css/notif-box.css",
+				"legacyCss": "https://sales-pop.carecart.io/lib/salesnotifier.css"
+			};
+		}
+
+		var tempAnchorTag = document.createElement('a');
+		tempAnchorTag.href = thisLibUrl;
+
+		return {
+			"backend": "https://" + tempAnchorTag.hostname + "/FrontController/",
+			"css": "https://" + tempAnchorTag.hostname + "/public/front_assets/new-ui/css/notif-box.css",
+			"legacyCss": "https://" + tempAnchorTag.hostname + "/lib/salesnotifier.css"
+		};
+	}
+
+	var serverUrl = getServerUrls();
 
 	var salespoplib_vars_obj = {
 		is_again: false,
@@ -692,7 +712,7 @@ scriptInjection("https://code.jquery.com/jquery-3.2.1.min.js", function () {
 		modal_id: "salesPopup",
 		modal_response_html: "",
 		is_allowed: false,
-		backend_url: backendUri,
+		backend_url: serverUrl.backend,
 		do_fire: true,
 		do_takeover_fire: true,
 		aw_list: '',
@@ -748,6 +768,7 @@ scriptInjection("https://code.jquery.com/jquery-3.2.1.min.js", function () {
 		show_relevant: 0,
 		do_restrict: 0,
 		display_time: 0,
+		isLegacyStore: 0,
 		desktop_position: "bottom left",
 		allNotifications: [],
 		allCollectionsWithProducts: [],
@@ -762,7 +783,7 @@ scriptInjection("https://code.jquery.com/jquery-3.2.1.min.js", function () {
 		salespoplib_vars_obj.checkDevice = 'desktop';
 	}
 
-	$jq321("head").append($jq321("<link/>", {rel: "stylesheet", href: cssUri}));
+	$jq321("head").append($jq321("<link/>", {rel: "stylesheet", href: serverUrl.css}));
 
 	// @todo Totally unneccessary. Just use window.location.hostname instead.
 	function extractHostname(url) {
@@ -1067,16 +1088,24 @@ scriptInjection("https://code.jquery.com/jquery-3.2.1.min.js", function () {
 	window.checkmodule_popup = function (response) {
 		apiResponse = response;
 
-		if (apiResponse.string === 'no_data') {
-			spDebuger.storeLog("There is no notification data.");
+		if ( apiResponse.hasOwnProperty('string') &&
+			( apiResponse.string === 'sales_notification_disabled' || apiResponse.string === 'no_data')
+		) {
+			spDebuger.storeLog("apiResponse.string: " + apiResponse.string);
 			return false;
 		}
 
+		if( apiResponse.hasOwnProperty('isLegacyStore') &&
+			parseInt(apiResponse.isLegacyStore) === 1 ) {
+            $jq321("head").append( $jq321("<link/>", {
+                rel: "stylesheet",
+                href: serverUrl.legacyCss + "?" + (new Date()).getTime()
+            }) );
+        }
+
 		if (parseInt(apiResponse.do_restrict) === 1) {
 			var notificationsAllowed = isNotificationAllowedOnCurrentPage();
-
 			spDebuger.storeLog("Notifications allowed: " + notificationsAllowed);
-
 			if (!notificationsAllowed) {
 				return false;
 			}
@@ -1133,7 +1162,7 @@ scriptInjection("https://code.jquery.com/jquery-3.2.1.min.js", function () {
 					popUpHasDisplayedCounter = 0;
 				}
 
-				if (salespoplib_vars_obj.triggered_count >= apiResponse.max_noti) {
+				if (salespoplib_vars_obj.triggered_count >= apiResponse.max_noti-1) {
 					clearInterval(popUpIntervalHandle);
 					return false;
 				}
