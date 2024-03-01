625650c555c4846901d3cdfc38ea99c064f1bf10
Naseem Abbas <120629585+naseemabbas14@users.noreply.github.com>
2024-03-01 12:26:48 +0500

    CTI-3227
    CTI-3227

--- salesnotifier.js
+++ salesnotifier.js
@@ -3,7 +3,7 @@
  * @author CareCart
  * @link https://apps.shopify.com/partners/care-cart
  * @link https://carecart.io/
- * @version 5.0.11
+ * @version 5.0.12
  *
  * Any unauthorized use and distribution of this and related files, is strictly forbidden.
  * In case of any inquiries, please contact here: https://carecart.io/contact-us/
@@ -98,10 +98,10 @@ scriptInjection("https://code.jquery.com/jquery-3.2.1.min.js", function () {
 
         var styles = {};
 
-        var coreStyle = {
+       var coreStyle = {
             name: "core",
-            html: "<div class=\"" + pluginClassName + "-wrapper\">\n	<div class=\"" + pluginClassName + "-arrow\"></div>\n	<div class=\"" + pluginClassName + "-container\"></div>\n</div>",
-            css: "." + pluginClassName + "-corner {\n	position: fixed;\n	margin: 30px;\n	z-index: 999999999999;\n}\n\n." + pluginClassName + "-corner ." + pluginClassName + "-wrapper,\n." + pluginClassName + "-corner ." + pluginClassName + "-container {\n	position: relative;\n	display: block;\n	height: inherit;\n	width: inherit;\n	margin: 3px;\n}\n\n." + pluginClassName + "-wrapper {\n	z-index: 1;\n	position: absolute;\n	display: inline-block;\n	height: 0;\n	width: 0;\n}\n\n." + pluginClassName + "-container {\n	display: none;\n	z-index: 1;\n	position: absolute;\n}\n\n." + pluginClassName + "-hidable {\n	cursor: pointer;\n}\n\n[data-notify-text],[data-notify-html] {\n	position: relative;\n}\n\n." + pluginClassName + "-arrow {\n	position: absolute;\n	z-index: 2;\n	width: 0;\n	height: 0;\n}"
+            html: "<div class=\"" + pluginClassName + "-wrapper\">\n <div class=\"" + pluginClassName + "-arrow\"></div>\n <div class=\"" + pluginClassName + "-container\"></div>\n</div>",
+            css: "." + pluginClassName + "-corner {\n position: fixed;\n margin: 30px;\n z-index: 999999999999;\n }\n\n." + pluginClassName + "-corner ." + pluginClassName + "-wrapper,\n." + pluginClassName + "-corner ." + pluginClassName + "-container {\n position: relative;\n display: block;\n height: inherit;\n width: inherit;\n margin: 3px;\n }\n\n." + pluginClassName + "-wrapper {\n z-index: 1;\n position: absolute;\n display: inline-block;\n height: 0;\n width: 0;\n }\n\n." + pluginClassName + "-container {\n display: none;\n z-index: 1;\n position: absolute;\n }\n\n." + pluginClassName + "-hidable {\n cursor: pointer;\n }\n\n[data-notify-text],[data-notify-html] {\n position: relative;\n }\n\n." + pluginClassName + "-arrow {\n position: absolute;\n z-index: 2;\n width: 0;\n height: 0;\n }\n\n@media only screen and (max-width: 600px) {\n ." + pluginClassName + "-corner {\n z-index: 999;\n }\n}"
         };
 
         var stylePrefixes = {
